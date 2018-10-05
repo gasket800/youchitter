c9c2dcbf4e91e2a344f7b6bf57f4ddfc2a02490f
gasket800 <junoyos@88e9fe7b9073.ant.amazon.com>
2018-10-05 18:16:10 +0900

    organized to push repo

--- getSearch.py
+++ getSearch.py
@@ -3,6 +3,9 @@
 import config
 import json
 from requests_oauthlib import OAuth1Session
+import sqlite3
+from sqlite3 import Error
+
 
 CK = config.CONSUMER_KEY
 CS = config.CONSUMER_SECRET
@@ -10,20 +13,41 @@ AT = config.ACCESS_TOKEN
 ATS = config.ACCESS_TOKEN_SECRET
 
 twitter = OAuth1Session(CK, CS, AT, ATS)
-
 url = 'https://api.twitter.com/1.1/search/tweets.json'
-
 word = 'うんこ'.decode('utf-8')
+params = {'count': 100, 'q': word}
 
-params = {'count': 10, 'q': word}
 res = twitter.get(url, params=params)
 
 if res.status_code == 200:
-    timelines = json.loads(res.text)
-    print timelines['statuses'][0]['created_at']
-    print timelines['statuses'][0]['text']
-    print '-' * 100
+    tweets = json.loads(res.text)
+
+    try:
+        conn = sqlite3.connect('boo_data.sqlite3')
+
+        for tweet in tweets['statuses']:
+            created_at = tweet['created_at']
+            text = tweet['text']
+            sql = "insert into boo_data (text, created_at) values ('%s', '%s')" % (text, created_at)
+            conn.execute(sql)
+
+            print created_at
+            print text
+            print '-' * 100
+
+        conn.commit()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
 else:
     print("Failed: %d" % res.status_code)
+
+
+
+
 
 
