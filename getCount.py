5a68f1e3e8684a832b0d781a1edd349d2c89021d
gasket800 <junoyos@88e9fe7b9073.ant.amazon.com>
2018-10-12 12:01:32 +0900

    add set_start_time & update count algorithm

--- getCount.py
+++ getCount.py
@@ -1,3 +1,4 @@
+import sys
 import sqlite3
 import datetime
 
@@ -9,28 +10,33 @@ data = conn.execute(sql)
 counts = []
 count = 0
 
-start_time = datetime.datetime.strptime('2018-10-05 12:20:00', '%Y-%m-%d %H:%M:%S')
-end_time = start_time + datetime.timedelta(0, 60)
+# set_start_time = raw_input('input start time as yyyy-mm-dd hh:mm >> ')
+set_start_time = sys.argv[1]
+
+try:
+    start_time = datetime.datetime.strptime(set_start_time, '%Y-%m-%d %H:%M')
+
+except Exception as e:
+    print e
+    sys.exit()
+
+delta_time = datetime.timedelta(0, 60)
+end_time = start_time + delta_time
 
 print start_time
 
 for i in data:
-    # print i
-    h1 = datetime.datetime.strptime(i[0], '%a %b %d %H:%M:%S +0000 %Y')
-    # h2 = h1 - start_time
-    # print h2 if h2 >= datetime.timedelta() else 'x'
-    # print h1 , h2 , datetime.timedelta(), datetime.timedelta(0, 60)
+    tweet_time = datetime.datetime.strptime(i[0], '%Y-%m-%d %H:%M:%S')
 
-    if start_time <= h1 < end_time:
+    if tweet_time < end_time:
         count += 1
 
-    if h1 >= end_time:
-        # counts.append(end_time count)
-        counts.append((end_time, count))
-        end_time = end_time + datetime.timedelta(0, 60)
-        count = 0
-
-    print start_time, h1, end_time, count
+    else:
+        while tweet_time >= end_time:
+            counts.append((end_time, count))
+            end_time = end_time + delta_time
+            count = 0
+        count = 1
 
 for k, v in counts:
     print k, v
