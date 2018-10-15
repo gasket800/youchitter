8ff708a7c24f915bf9a978e80c516f4c1931d270
gasket800 <junoyos@88e9fe7b9073.ant.amazon.com>
2018-10-15 10:55:30 +0900

    update s3uploader.py added upload of booTweetcount.data

--- s3uploader.py
+++ s3uploader.py
@@ -4,3 +4,4 @@ bucket_name = 'junoyos'
 s3 = boto3.resource('s3')
 
 s3.Bucket(bucket_name).upload_file('./booTweetcount.png', 'booTweetcount.png')
+s3.Bucket(bucket_name).upload_file('./booTweetcount.data', 'booTweetcount.data')
